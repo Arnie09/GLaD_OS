e02683e05dfb1b03a3f70df5e2e7c531e10d8fcf
Arnab Chanda <arnabchanda964@gmail.com>
2019-07-02 22:06:47 +0530

    Voice recorder working well

--- AndroidApp/GLaD_OS/app/src/main/java/com/example/glad_os/MainActivity.java
+++ AndroidApp/GLaD_OS/app/src/main/java/com/example/glad_os/MainActivity.java
@@ -1,8 +1,17 @@
 package com.example.glad_os;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.speech.RecognitionListener;
+import android.speech.RecognizerIntent;
+import android.speech.SpeechRecognizer;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.util.Log;
 import android.view.View;
 import android.support.v4.view.GravityCompat;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -13,9 +22,26 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener ,RecognitionListener {
+
+    private static final int REQUEST_RECORD_PERMISSION = 100;
+    private TextView returnedText;
+    private String currentPartialText;
+    private String currentConfirmedText;
+    private SpeechRecognizer speech = null;
+    private Intent recognizerIntent;
+    private String LOG_TAG = "SpeechRecognizer";
+    Boolean recordButtonStatus;
+    FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +49,22 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
+        fab = findViewById(R.id.fab);
 
+        returnedText = (TextView) findViewById(R.id.resultText);
+        recordButtonStatus = false;
+        currentPartialText = "";
+        currentConfirmedText = "";
+
+        speech = SpeechRecognizer.createSpeechRecognizer(this);
+        Log.i(LOG_TAG, "isRecognitionAvailable: " + SpeechRecognizer.isRecognitionAvailable(this));
+        speech.setRecognitionListener(this);
+        recognizerIntent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_PREFERENCE,
+                "en");
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        recognizerIntent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 3);
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,8 +73,19 @@ public class MainActivity extends AppCompatActivity
                 /* Trigger voice
                 recording here
                  */
-
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                if(recordButtonStatus){
+                    recordButtonStatus = false;
+                    speech.stopListening();
+                    fab.setBackground(getDrawable(R.drawable.microphone_button_off));
+                }else{
+                    ActivityCompat.requestPermissions
+                            (MainActivity.this,
+                                    new String[]{Manifest.permission.RECORD_AUDIO},
+                                    REQUEST_RECORD_PERMISSION);
+                    recordButtonStatus = true;
+                    fab.setBackground(getDrawable(R.drawable.microphone_button_on));
+                }
+                Snackbar.make(view, "Recording audio", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
@@ -96,5 +147,146 @@ public class MainActivity extends AppCompatActivity
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+    /*functions of the
+    Speech Recogniser class
+     */
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case REQUEST_RECORD_PERMISSION:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    speech.startListening(recognizerIntent);
+                } else {
+                    Toast.makeText(MainActivity.this, "Permission Denied!", Toast
+                            .LENGTH_SHORT).show();
+                }
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (speech != null) {
+            recordButtonStatus = false;
+            fab.setBackground(getDrawable(R.drawable.microphone_button_off));
+            Log.i(LOG_TAG, "destroy");
+        }
+    }
+
+
+    @Override
+    public void onBeginningOfSpeech() {
+        Log.i(LOG_TAG, "onBeginningOfSpeech");
+    }
+
+    @Override
+    public void onBufferReceived(byte[] buffer) {
+        Log.i(LOG_TAG, "onBufferReceived: " + buffer);
+    }
+
+    @Override
+    public void onEndOfSpeech() {
+        Log.i(LOG_TAG, "onEndOfSpeech");
+
+        recordButtonStatus = false;
+        fab.setBackground(getDrawable(R.drawable.microphone_button_off));
+    }
+
+    @Override
+    public void onError(int errorCode) {
+        String errorMessage = getErrorText(errorCode);
+        Log.d(LOG_TAG, "FAILED " + errorMessage);
+        Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show();
+        recordButtonStatus = false;
+        fab.setBackground(getDrawable(R.drawable.microphone_button_off));
+    }
+
+    @Override
+    public void onEvent(int arg0, Bundle arg1) {
+        Log.i(LOG_TAG, "onEvent");
+
+    }
+
+    @Override
+    public void onPartialResults(Bundle arg0) {
+        Log.i(LOG_TAG, "Results");
+
+    }
+
+    @Override
+    public void onReadyForSpeech(Bundle arg0) {
+        Log.i(LOG_TAG, "Ready For Speech");
+    }
+
+    @Override
+    public void onResults(Bundle results) {
+        Log.i(LOG_TAG, "Results");
+        returnedText.setText("");
+        ArrayList<String> matches = results
+                .getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION);
+
+
+        String text = matches.get(0);
+        returnedText.setText(text);
+
+        recordButtonStatus = false;
+        fab.setBackground(getDrawable(R.drawable.microphone_button_off));
+    }
+
+    @Override
+    public void onRmsChanged(float rmsdB) {
+        Log.i(LOG_TAG, "RMS Changed: " + rmsdB);
+
+    }
+
+    public static String getErrorText(int errorCode) {
+        String message;
+        switch (errorCode) {
+            case SpeechRecognizer.ERROR_AUDIO:
+                message = "ERROR: There was an error recording audio.";
+                break;
+            case SpeechRecognizer.ERROR_CLIENT:
+                message = "ERROR: There was an error with the Client.";
+                break;
+            case SpeechRecognizer.ERROR_INSUFFICIENT_PERMISSIONS:
+                message = "ERROR:  You need to accept permissions first.  Please go to your phone Settings -> Apps -> Speech to Text and accept.";
+                break;
+            case SpeechRecognizer.ERROR_NETWORK:
+                message = "ERROR:  There was a Network error";
+                break;
+            case SpeechRecognizer.ERROR_NETWORK_TIMEOUT:
+                message = "ERROR: There was a Network timeout";
+                break;
+            case SpeechRecognizer.ERROR_NO_MATCH:
+                message = "ERROR: I didn't quite catch that.";
+                break;
+            case SpeechRecognizer.ERROR_RECOGNIZER_BUSY:
+                message = "ERROR: RecognitionService busy";
+                break;
+            case SpeechRecognizer.ERROR_SERVER:
+                message = "ERROR:  A Server error occurred";
+                break;
+            case SpeechRecognizer.ERROR_SPEECH_TIMEOUT:
+                message = "ERROR: I didn't quite catch that";
+                break;
+            default:
+                message = "Hmm, I'm not sure I understand, please try again.";
+                break;
+        }
+        return message;
     }
 }
