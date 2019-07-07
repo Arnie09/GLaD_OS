5fc9947f2ac4206931f19cc8d583452f289527cd
Arnab Chanda <arnabchanda964@gmail.com>
2019-07-07 20:41:32 +0530

    database deleting working properly

--- AndroidApp/GLaD_OS/app/src/main/java/com/example/glad_os/HistoryActivity.java
+++ AndroidApp/GLaD_OS/app/src/main/java/com/example/glad_os/HistoryActivity.java
@@ -4,14 +4,23 @@ import android.database.Cursor;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class HistoryActivity extends AppCompatActivity {
 
@@ -20,13 +29,37 @@ public class HistoryActivity extends AppCompatActivity {
     ArrayList<Integer> id;
     MyCustomAdapter customAdapter;
     ListView listView;
+    View previous_view;
+    int ID_TO_BE_DELETED = -1;
+    View currentselectedrow;
+    DatabaseHandler databaseHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_history);
+
+        Toolbar toolbar = findViewById(R.id.toolbar_history);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
         listView = findViewById(R.id.ListView);
-        DatabaseHandler databaseHandler = new DatabaseHandler(this);
+        databaseHandler = new DatabaseHandler(this);
+
+        refreshListView();
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long idno) {
+                ID_TO_BE_DELETED = id.get(position);
+                currentselectedrow = view;
+                highlightCurrentRow(currentselectedrow);
+                return false;
+            }
+        });
+    }
+    private  void refreshListView(){
         Cursor result = databaseHandler.getallData();
         if (result.getCount() == 0) {
             Log.i("MAIN_ACTIVITY", "NO DATA");
@@ -45,6 +78,45 @@ public class HistoryActivity extends AppCompatActivity {
             listView.setAdapter(customAdapter);
         }
     }
+
+    private void highlightCurrentRow(View currentselectedview) {
+        if(previous_view == null) {
+            currentselectedview.setBackgroundColor(Color.GRAY);
+            previous_view = currentselectedview;
+        }
+        else{
+            previous_view.setBackgroundColor(Color.parseColor("#00000000"));
+            currentselectedview.setBackgroundColor(Color.GRAY);
+            previous_view = currentselectedview;
+        }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.history_options_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.action_delete){
+            //delete database entry
+            Integer res = databaseHandler.deleteData(String.valueOf(ID_TO_BE_DELETED));
+            if (res != 0) {
+                refreshListView();
+                Toast.makeText(this, "Item Deleted", Toast.LENGTH_SHORT).show();
+                ID_TO_BE_DELETED = -1;
+            } else {
+                refreshListView();
+                Toast.makeText(this, "Select something before trying to delete", Toast.LENGTH_LONG).show();
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     class MyCustomAdapter extends BaseAdapter {
 
         @Override
